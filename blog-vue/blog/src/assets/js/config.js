35bc2d65b8edb522fe4fa4016817e3bc942f7c24
lee <904688436@qq.com>
2022-09-08 23:13:29 +0800

    springboot + vue 博客前端版本

---
+++ blog-vue/blog/src/assets/js/config.js
@@ -0,0 +1,7 @@
+export default {
+  QQ_APP_ID: "102021010",
+  QQ_REDIRECT_URI: "https://fengxiuyang.cn/oauth/login/qq",
+  WEIBO_APP_ID: "1791833375",
+  WEIBO_REDIRECT_URI: "https://fengxiuyang.cn/oauth/login/weibo",
+  TENCENT_CAPTCHA: "2088053498"
+};
