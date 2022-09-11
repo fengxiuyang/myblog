5107478d31f2d822c13889e9994f04e320a9bf8a
lee <904688436@qq.com>
2022-09-11 19:20:05 +0800

    配置文件 + springboot启动类

---
+++ blog-springboot/src/main/java/com/lee/blog/BlogApplication.java
@@ -0,0 +1,16 @@
+package com.lee.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @date: 2022/9/11 17:25
+ * @author: zhicheng lee
+ */
+
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class,args);
+    }
+}
