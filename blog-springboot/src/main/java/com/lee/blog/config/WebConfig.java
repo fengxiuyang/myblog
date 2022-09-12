a2279c5961b53d40f43a16b94f164979fff7857c
lee <904688436@qq.com>
2022-09-12 20:49:06 +0800

    配置springboot跨域访问

---
+++ blog-springboot/src/main/java/com/lee/blog/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.lee.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author: zhicheng lee
+ * @date: 2022/9/12 20:16
+ */
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 设置允许跨域的路径
+        registry.addMapping("/**")
+                // 设置允许跨域请求的域名
+                .allowedOriginPatterns("*")
+                // 是否允许cookie
+                .allowCredentials(true)
+                // 设置允许的请求方式
+                .allowedMethods("*")
+                // 设置允许的header属性
+                .allowedHeaders("*")
+                // 跨域允许时间
+                .maxAge(3600);
+    }
+}
