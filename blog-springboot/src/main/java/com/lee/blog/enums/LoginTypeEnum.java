dd17d7d831a9fe878ab2b4e4ee9462fb4386892c
lee <904688436@qq.com>
2022-09-17 10:34:09 +0800

    添加UserAuth实体类、RoleEnum角色枚举、LoginTypeEnum登陆方式枚举

---
+++ blog-springboot/src/main/java/com/lee/blog/enums/LoginTypeEnum.java
@@ -0,0 +1,45 @@
+package com.lee.blog.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 登陆方式枚举
+ *
+ * @author: zhicheng lee
+ * @date: 2022/9/17 10:32
+ */
+
+@Getter
+@AllArgsConstructor
+public enum LoginTypeEnum {
+    /**
+     * 邮箱登录
+     */
+    EMAIL(1, "邮箱登录", ""),
+    /**
+     * QQ登录
+     */
+    QQ(2, "QQ登录", "qqLoginStrategyImpl"),
+    /**
+     * 微博登录
+     */
+    WEIBO(3, "微博登录", "weiboLoginStrategyImpl");
+
+    /**
+     * 登录方式
+     */
+    private final Integer type;
+
+    /**
+     * 描述
+     */
+    private final String desc;
+
+    /**
+     * 策略
+     */
+    private final String strategy;
+
+
+}
