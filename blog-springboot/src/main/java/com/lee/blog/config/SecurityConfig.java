0fc2a003f534c7a0053ba88a62105ee9f75a6be0
lee <904688436@qq.com>
2022-09-17 23:11:30 +0800

    配置spring security

---
+++ blog-springboot/src/main/java/com/lee/blog/config/SecurityConfig.java
@@ -0,0 +1,73 @@
+package com.lee.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * spring security配置类
+ *
+ * @author: zhicheng lee
+ * @date: 2022/9/17 17:32
+ */
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * 配置加密方式
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 配置权限
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                // 关闭csrf，防止csrf攻击
+                .csrf().disable()
+                // 使用token认证，不使用session，因此关闭session
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // 配置请求相关的认证
+                .authorizeRequests()
+                // 登陆接口允许匿名访问
+                .antMatchers("/login").anonymous()
+                // 其它全部请求，不需要认证即可访问
+                .anyRequest().permitAll();
+
+        // // 添加过滤器
+        // http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // // 配置异常处理器
+        // http.exceptionHandling()
+        //         // 配置认证失败处理器
+        //         .authenticationEntryPoint(authenticationEntryPoint)
+        //         // 配置权限不足处理器
+        //         .accessDeniedHandler(accessDeniedHandler);
+
+        //关闭默认的注销功能
+        http.logout().disable();
+
+        // 允许跨域
+        http.cors();
+    }
+
+    /**
+     * 引入AuthenticationManager接口，用于认证Authentication的方法
+     */
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
