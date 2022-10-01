d15663d4b6cfcee26d5a0fb747685ddb9ea5e0ad
lee <904688436@qq.com>
2022-10-01 22:12:41 +0800

    在RoleController中，添加获取角色信息功能

--- blog-springboot/src/main/java/com/lee/blog/controller/RoleController.java
+++ blog-springboot/src/main/java/com/lee/blog/controller/RoleController.java
@@ -5,6 +5,7 @@ import com.lee.blog.service.RoleService;
 import com.lee.blog.vo.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,9 +29,18 @@ public class RoleController {
      * 角色列表
      */
     @GetMapping("/listAllRole")
-    public ResponseResult listAllRole(){
+    public ResponseResult listAllRole() {
         List<Role> roles = roleService.selectRoleAll();
         return ResponseResult.okResult(roles);
+    }
+
+    /**
+     * 获取角色信息
+     */
+    @GetMapping(value = "/{roleId}")
+    public ResponseResult getInfo(@PathVariable Long roleId) {
+        Role role = roleService.getById(roleId);
+        return ResponseResult.okResult(role);
     }
 
 }
