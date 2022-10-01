e4b3e545fb934cc3e11406c1a454262e7745e846
lee <904688436@qq.com>
2022-10-01 21:06:16 +0800

    在MenuController中，添加条件查询菜单列表功能

--- blog-springboot/src/main/java/com/lee/blog/controller/MenuController.java
+++ blog-springboot/src/main/java/com/lee/blog/controller/MenuController.java
@@ -2,8 +2,10 @@ package com.lee.blog.controller;
 
 import com.lee.blog.entity.Menu;
 import com.lee.blog.service.MenuService;
+import com.lee.blog.util.BeanCopyUtils;
 import com.lee.blog.util.SystemConverter;
 import com.lee.blog.vo.MenuTreeVo;
+import com.lee.blog.vo.MenuVo;
 import com.lee.blog.vo.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +27,16 @@ public class MenuController {
 
     @Autowired
     private MenuService menuService;
+
+    /**
+     * 条件查询菜单列表
+     */
+    @GetMapping("/list")
+    public ResponseResult list(Menu menu) {
+        List<Menu> menus = menuService.selectMenuList(menu);
+        List<MenuVo> menuVos = BeanCopyUtils.copyBeanList(menus, MenuVo.class);
+        return ResponseResult.okResult(menuVos);
+    }
 
     /**
      * 获取菜单下拉树列表
